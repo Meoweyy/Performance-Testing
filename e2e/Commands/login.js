b2877599b73faf9cc0397289833e6fc5e592c098
Meoweyyy <Ethantpy@gmail.com>
2025-07-02 18:15:56 +0800

    updated 2025_07_02

--- e2e/Commands/login.js
+++ e2e/Commands/login.js
@@ -5,9 +5,9 @@ async function SN_login(page){
     await page.fill('#user_name',"aes.creator");
     await page.fill('#user_password', "oO$oOc$OX5l3");
     await page.click('#sysverb_login');
-    await expect(page).toHaveURL(/login*_redirect.do/i);
-    await expect(page).toHaveURL(/now.*nav.*ui/i, {timeout: 15000});
-   // await expect(page.getByRole('button', { name: 'My ServiceNow landing page' }, {timeout: 15000})).toBeVisible();
+    await expect(page).toHaveURL(/login*_redirect.do/i, {timeout: 120000});
+    await expect(page).toHaveURL(/now.*nav.*ui/i, {timeout: 120000});
+//    await expect(page.getByRole('button', { name: 'My ServiceNow landing page' }, {timeout: 30000})).toBeVisible();
 }
 
-module.exports = { SN_login }; 
+module.exports ={ SN_login }; // Export the function for use in other files
--- e2e/SN_test.spec.js
+++ e2e/SN_test.spec.js
@@ -1,5 +1,5 @@
 const { test, expect } = require("@playwright/test");
-const SN_login = require("./artillery_tests/login.js"); // Use require for CommonJS modules
+const { SN_login } = require("./Commands/login.js"); // Use require for CommonJS modules
 
 test('test', async ({ page }) => {
   await SN_login(page);
